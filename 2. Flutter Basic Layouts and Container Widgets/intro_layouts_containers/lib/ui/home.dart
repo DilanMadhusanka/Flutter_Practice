5c695ad29916a7359ee723798c47071ffd28c35b
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2019-10-29 23:25:23 +0530

    Flutter Basic Layouts and Container Widgets

---
+++ 2. Flutter Basic Layouts and Container Widgets/intro_layouts_containers/lib/ui/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      color: Colors.greenAccent,
+      alignment: Alignment.center,
+
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Text("First item", textDirection: TextDirection.ltr,
+          style: new TextStyle(color: Colors.white),),
+
+          new Text("Second item", textDirection: TextDirection.ltr,
+          style: new TextStyle(color: Colors.blue),),
+
+          new Container(
+            color: Colors.deepOrange.shade50,
+            alignment: Alignment.bottomLeft,
+
+            child: new Text("Third item", textDirection: TextDirection.ltr,
+                      style: new TextStyle(color: Colors.blue),),
+          )
+        ],
+      )
+    );
+  }
+
+}
